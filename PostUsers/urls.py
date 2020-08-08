9ef5926f794646c0ab3ffd94cd6f85981adb08d6
srtushar887 <sr.tushar.pro@gmail.com>
2020-08-09 03:35:39 +0600

    report added

--- PostUsers/urls.py
+++ PostUsers/urls.py
@@ -7,4 +7,14 @@ urlpatterns = [
     path('user/delete/<str:uid>',views.deleteusers,name="deleteusers"),
     path('user/posts/',views.userposts,name="userposts"),
     path('user/post-details/<str:post_id>',views.postview,name="postview"),
+    path('user/post-delete/',views.postdelete,name="postdelete"),
+    # category
+    path('categories/',views.category,name="category"),
+    path('category/save/',views.categorysave,name="categorysave"),
+    path('category/update/',views.updatecategory,name="updatecategory"),
+    path('category/delete/',views.deletecategory,name="deletecategory"),
+    # video report
+     path('video/reports',views.videoreport,name="videoreport"),
+
+
 ]
--- PostUsers/views.py
+++ PostUsers/views.py
@@ -69,3 +69,102 @@ def postview(request,post_id):
             comment_list.append(comment)
         print(comment_list)
         return render (request, "dashboard/postdetails.html", {"data": postdetail,"likes":len(like_list),"commets":comment_list})
+
+
+
+
+
+del_post = db.collection('post')
+del_comment = db.collection('user_comment')
+docfile1 = db.collection('post')
+def postdelete(request ):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            postid = request.POST['postdeleteid']
+            getData = docfile1.document(postid).get()
+            getData1  = getData.to_dict() 
+
+            getcomment = del_comment.get()
+            comar = []
+            for i in getcomment:
+                comment = i.to_dict()
+                if comment['id'] == getData1['id']:
+                    comar.append(comment)
+                    i.reference.delete()
+                    
+                    
+            getData.reference.delete()
+
+            # print(getData1)
+            return redirect('userposts')
+
+
+docfilecategory = db.collection('categories')
+def category(request):
+    if request.user.is_authenticated:
+        getData = docfilecategory.get()
+        case_list = []
+        for i in getData:
+            stored = i.to_dict()
+            stored['docid']=i.id
+            case_list.append(stored)
+        return render(request,'dashboard/category.html',{"data": case_list})
+    else:
+        return redirect('login')
+
+save_cat = db.collection('categories')
+def categorysave(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            cat = request.POST['category_name']
+            data = {
+                'category_name' : cat,
+            }
+            db.collection(u'categories').document().set(data)
+    
+            return redirect('category')
+    else:
+        return redirect('login')
+
+update_cat = db.collection('categories')
+def updatecategory(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            cat = request.POST['category_name']
+            catupid = request.POST['catdeleteid']
+            data = {
+                'category_name' : cat,
+            }
+            db.collection(u'categories').document(catupid).update(data)
+    
+            return redirect('category')
+    else:
+        return redirect('login')
+
+
+delete_cat = db.collection('categories')
+def deletecategory(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            catdelid = request.POST['catdeleteid']
+            getData = delete_cat.document(catdelid).get()
+            getData.reference.delete()
+            return redirect('category')
+    else:
+        return redirect('login')
+
+
+docfilereport = db.collection('report')
+def videoreport(request):
+    if request.user.is_authenticated:
+        getData = docfilereport.get()
+        case_list1 = []
+        for i in getData:
+            stored = i.to_dict()
+            stored['docid']=i.id
+            case_list1.append(stored)
+            return render(request,'dashboard/report.html',{"data": case_list1})
+            
+    else:
+        return redirect('login')
+            
